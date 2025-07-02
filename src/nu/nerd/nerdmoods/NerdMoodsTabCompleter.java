49342aa61a16c399e10d1a32541dac115d7caf33
Mats1000 <mathewshifflett@gmail.com>
2025-07-02 14:54:48 -0400

    Added Reset to TabCompleter.

--- src/nu/nerd/nerdmoods/NerdMoodsTabCompleter.java
+++ src/nu/nerd/nerdmoods/NerdMoodsTabCompleter.java
@@ -16,18 +16,20 @@ public class NerdMoodsTabCompleter implements TabCompleter {
                                                 @NotNull String alias,
                                                 String @NotNull [] args) {
         List<String> completions = new ArrayList<>();
-        if (command.getName().equalsIgnoreCase("ptime")) {
-            if (args.length == 1) {
-                String arg = args[0].toLowerCase();
-                if ("day".startsWith(arg)) completions.add("day");
-                if ("night".startsWith(arg)) completions.add("night");
-                // You can also add a placeholder for numbers or nothing here
-            }
-        } else if (command.getName().equalsIgnoreCase("prain")) {
-            if (args.length == 1) {
-                String arg = args[0].toLowerCase();
-                if ("on".startsWith(arg)) completions.add("on");
-                if ("off".startsWith(arg)) completions.add("off");
+        if (args.length == 1) {
+            String arg = args[0].toLowerCase();
+
+            switch (command.getName().toLowerCase()) {
+                case "ptime" -> {
+                    if ("day".startsWith(arg)) completions.add("day");
+                    if ("night".startsWith(arg)) completions.add("night");
+                    if ("reset".startsWith(arg)) completions.add("reset");
+                }
+                case "prain" -> {
+                    if ("on".startsWith(arg)) completions.add("on");
+                    if ("off".startsWith(arg)) completions.add("off");
+                    if ("reset".startsWith(arg)) completions.add("reset");
+                }
             }
         }
         return completions;
